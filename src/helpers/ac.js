0b33b96821a038e0bc7893ef46dffdad3b03cbf3
NestorNavarro <nestor.navarroarizga@gmail.com>
2022-08-25 11:25:24 -0500

    comment deleted

--- src/helpers/ac.js
+++ src/helpers/ac.js
@@ -12,35 +12,6 @@ const grantsObject = {
 	},
 };
 
-// const permission = {
-// 	users : {
-// 		create : {
-// 			admin      : ["*"], //att
-// 			supervisor : ["*"],
-// 		},
-// 		read : {
-// 			admin      : ["*"],
-// 			supervisor : ["name"],
-// 		},
-// 		update : {
-// 			admin      : ["*"],
-// 			supervisor : ["*"],
-// 		},
-// 		delete : {
-// 			admin      : ["*"],
-// 			supervisor : ["*"],
-// 		},
-// 	},
-// };
-
-
-//admin todo
-//supervisor pede leer el expediente, !tabla de ordenes.
-
-//[0] = *
-//si no, filtrar los atributos.
-
-
 const ac = new AccessControl(grantsObject);
 
 export default ac;
